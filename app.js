1d3e6a725f7e51e3f164e2cb6a7c86fb3fc4a4bb
Minh Quang <buiquangminh@gmail.com>
2022-10-11 00:35:52 +0700

    change link cdn in admin views

--- app.js
+++ app.js
@@ -62,11 +62,11 @@ var app = express();
 
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', './views');
+
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
 //app.use('/public', express.static('public'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
