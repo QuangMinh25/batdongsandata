65e157b4e8cede152110ff4999a8550541b0c6b7
Minh Quang <buiquangminh@gmail.com>
2022-11-02 21:43:35 +0700

    update heroku config

--- user/user.route.js
+++ user/user.route.js
@@ -6,8 +6,10 @@ const router = express.Router();
 
 let storage = multer.diskStorage({
     destination: function (req, file, callback) {
+
         console.log("file", file);
         callback(null, "./Uploads/");
+        
     },
     filename: function (req, file, callback) {
         // console.log("multer file:", file);
